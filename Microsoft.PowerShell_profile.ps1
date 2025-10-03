1d7eaec4a5d717b17968beefcf6420c4b29c57ac
Maciej Krosta <102286223+MK6554@users.noreply.github.com>
2025-10-03 11:07:59 +0200

    Fixed missing semicolon

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount 1 -A
 } | Out-Null
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1  # Don't show Python venv (posh already does it)
 $env:POSH_GIT_ENABLED = $false
-function Measure-Profile([string]$path = $profile) { Write-Host "Measuring $path" pwsh -NoProfile -Command "measure-Script -Path $path"}
+function Measure-Profile([string]$path = $profile) { Write-Host "Measuring $path"; pwsh -NoProfile -Command "measure-Script -Path $path"}
 function touch { [CmdletBinding()] param ([Parameter(Mandatory)][ValidateLength(1, [int]::MaxValue)][Alias('Path')][string]$LiteralPath, [Parameter()][switch]$AccessOnly, [Parameter()][switch]$ModificationOnly ) if (-not (Test-Path $LiteralPath)) { $null = New-Item -ItemType File -Path $LiteralPath }; $item = Get-Item $LiteralPath; if (-not $ModificationOnly.IsPresent) { $item.LastAccessTime = [datetime]::now }; if (-not $AccessOnly.IsPresent) { $item.LastWriteTime = [datetime]::now }; Get-Item $LiteralPath}
 function y { $tmp = [System.IO.Path]::GetTempFileName(); yazi $args --cwd-file="$tmp"; $cwd = Get-Content -Path $tmp -Encoding UTF8; if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) { Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd)) }; Remove-Item -Path $tmp }
 function qq { if ($env:YAZI_LEVEL) { exit } else { Write-error "No Yazi instance detected" } }
