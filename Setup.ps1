320fca3f600c4402c8ea024e16997838546ed206
Maciej Krosta <102286223+MK6554@users.noreply.github.com>
2025-10-03 14:25:12 +0200

    Expanded setup script

--- Setup.ps1
+++ Setup.ps1
@@ -1,3 +1,31 @@
-$gitconfigPath = Get-Item $psscriptroot/myconfig.gitconfig | % FullName
-Write-Host "Including file '$gitconfigPath' in the global git configuration."
-git config --global include.path $gitconfigPath
+#!/usr/bin/env pwsh
+$git = Get-Command git
+if(-not $git){
+	Write-Error "Git is not installed!!!"
+}else{
+	$gitconfigPath = Get-Item $psscriptroot/myconfig.gitconfig | % FullName
+	Write-Host "Including file '$gitconfigPath' in the global git configuration... " -NoNewLine -Foreground Green
+	git config --global include.path $gitconfigPath
+	Write-Host "DONE" -Foreground Green
+}
+$omp = Get-Command oh-my-posh -ea SilentlyContinue
+if(-not $omp){
+Write-host "Install oh-my-posh..." -Foreground Green
+	if($PSVersionTable.OS -like '*windows*'){
+		Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+	}else{
+		curl -s https://ohmyposh.dev/install.sh | bash -s
+	}
+}else{
+	Write-host "Upgrading oh-my-posh..." -Foreground Green
+	oh-my-posh upgrade
+}
+Write-Host "Installing font..." -Foreground Green
+oh-my-posh font install FantasqueSansMono
+Write-Host "Trusting PSGallery..." -Foreground Green
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+$modules = @('Terminal-Icons','Posh','PSProfiler')
+foreach ($mod in $modules){
+	Write-Host "Installing $mod..." -Foreground Green
+	Install-Module -Name $mod
+}
