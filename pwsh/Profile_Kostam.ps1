ab26893d1bbc8960191199a889a962ffdc215b5e
Maciej Krosta <102286223+MK6554@users.noreply.github.com>
2025-10-13 08:44:04 +0200

    Fixed zoxide

--- pwsh/Profile_Kostam.ps1
+++ pwsh/Profile_Kostam.ps1
@@ -1,13 +1,14 @@
 oh-my-posh init pwsh --config "$PSScriptRoot/../oh-my-posh/kostamfive.omp.json" | Invoke-Expression
 $global:IdleEventCounter = 0
 $global:EventCommands = @(
+  (& { (zoxide init powershell | Out-String) })
   'Set-PSReadLineKeyHandler -Chord tab -Function MenuComplete'
   'Import-Module -Name Terminal-Icons -Global'
   'Import-Module -Name Posh -Global'
 )
 Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount $global:EventCommands.Count -Action {
   $cmd = $global:EventCommands[$global:IdleEventCounter]
-  $cmd | Invoke-Expression
+  $cmd | Invoke-Expression 
   $global:IdleEventCounter = $global:IdleEventCounter + 1
   if ($global:IdleEventCounter -ge $eventCommands.Count) {
     Remove-Variable -Scope Global -Name 'IdleEventCounter'
@@ -20,6 +21,4 @@ function touch { [CmdletBinding()] param ([Parameter(Mandatory)][ValidateLength(
 function y { $tmp = [System.IO.Path]::GetTempFileName(); yazi $args --cwd-file="$tmp"; $cwd = Get-Content -Path $tmp -Encoding UTF8; if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) { Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd)) }; Remove-Item -Path $tmp }
 function qq { if ($env:YAZI_LEVEL) { exit } else { Write-Error 'No Yazi instance detected' } }
 function Get-PoshUpdates { Push-Location $profile/..; git pull; oh-my-posh upgrade; Pop-Location }
-if (Get-Command zoxide -ea SilentlyContinue) {
-  Invoke-Expression (& { (zoxide init powershell | Out-String) })
-}
+#Invoke-Expression (& { (zoxide init powershell | Out-String) }) -EA SilentlyContinue
