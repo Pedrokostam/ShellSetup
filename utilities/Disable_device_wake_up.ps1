cddb3993a29598d283ed86c18d1bbc11755af0b6
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2026-01-03 12:54:42 +0100

    improved usb-wake-up disabler

--- utilities/Disable_device_wake_up.ps1
+++ utilities/Disable_device_wake_up.ps1
@@ -1,3 +1,16 @@
 #Requires -RunAsAdministrator
 
-powercfg -devicequery wake_armed | % {powercfg -devicedisablewake $_}
+$devices = powercfg -devicequery wake_armed | ForEach-Object { $_.Trim()} | Where-Object Length
+
+if ($devices){
+   foreach ($device in $devices){
+      Write-host "Disabling ".. -NoNewline
+      Write-host $device -NoNewline -ForegroundColor Cyan
+      Write-host "... " -NoNewline
+      powercfg -devicedisablewake $device
+      Write-host "DONE" -ForegroundColor Green
+   }
+}
+else{
+   Write-host "No devices can wake-up the computer" -ForegroundColor Green
+}
