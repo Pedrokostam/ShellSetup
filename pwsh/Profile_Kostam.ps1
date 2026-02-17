077f8f4f42756736be8eecd4e39f8cd36f273946
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2026-02-17 09:59:20 +0100

    added get-shortcuttarget

--- pwsh/Profile_Kostam.ps1
+++ pwsh/Profile_Kostam.ps1
@@ -1,27 +1,29 @@
 oh-my-posh init pwsh --config "$PSScriptRoot/../oh-my-posh/kostamfive.omp.json" | Invoke-Expression
 $global:IdleEventCounter = 0
 $global:EventCommands = @(
-  (& { (zoxide init powershell | Out-String) })
-  'Set-PSReadLineKeyHandler -Chord tab -Function MenuComplete'
-  'Import-Module -Name Terminal-Icons -Global'
-  'Import-Module -Name Posh -Global'
+   (& { (zoxide init powershell | Out-String) })
+   'Set-PSReadLineKeyHandler -Chord tab -Function MenuComplete'
+   'Import-Module -Name Terminal-Icons -Global'
+   'Import-Module -Name Posh -Global'
 )
 Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount $global:EventCommands.Count -Action {
-  $cmd = $global:EventCommands[$global:IdleEventCounter]
-  $cmd | Invoke-Expression 
-  $global:IdleEventCounter = $global:IdleEventCounter + 1
-  if ($global:IdleEventCounter -ge $eventCommands.Count) {
-    Remove-Variable -Scope Global -Name 'IdleEventCounter'
-    Remove-Variable -Scope Global -Name 'EventCommands'
-  }
+   $cmd = $global:EventCommands[$global:IdleEventCounter]
+   $cmd | Invoke-Expression 
+   $global:IdleEventCounter = $global:IdleEventCounter + 1
+   if ($global:IdleEventCounter -ge $eventCommands.Count)
+   {
+      Remove-Variable -Scope Global -Name 'IdleEventCounter'
+      Remove-Variable -Scope Global -Name 'EventCommands'
+   }
 } | Out-Null
 $env:POSH_GIT_ENABLED = $false
-function Measure-Profile([string]$path = $profile) { Import-Module PSProfiler; Write-Host "Measuring $path"; pwsh -NoProfile -Command "measure-Script -Path $path" }
-function touch { [CmdletBinding()] param ([Parameter(Mandatory)][ValidateLength(1, [int]::MaxValue)][Alias('Path')][string]$LiteralPath, [Parameter()][switch]$AccessOnly, [Parameter()][switch]$ModificationOnly ) if (-not (Test-Path $LiteralPath)) { $null = New-Item -ItemType File -Path $LiteralPath }; $item = Get-Item $LiteralPath; if (-not $ModificationOnly.IsPresent) { $item.LastAccessTime = [datetime]::now }; if (-not $AccessOnly.IsPresent) { $item.LastWriteTime = [datetime]::now }; Get-Item $LiteralPath }
-function y { $tmp = [System.IO.Path]::GetTempFileName(); yazi $args --cwd-file="$tmp"; $cwd = Get-Content -Path $tmp -Encoding UTF8; if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) { Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd)) }; Remove-Item -Path $tmp }
-function qq { if ($env:YAZI_LEVEL) { exit } else { Write-Error 'No Yazi instance detected' } }
-function Get-PoshUpdates { Push-Location $profile/..; git pull; oh-my-posh upgrade; Pop-Location }
-function Watch-Command {
+# function Measure-Profile([string]$path = $profile) { Import-Module PSProfiler; Write-Host "Measuring $path"; pwsh -NoProfile -Command "measure-Script -Path $path" }
+# function touch { [CmdletBinding()] param ([Parameter(Mandatory)][ValidateLength(1, [int]::MaxValue)][Alias('Path')][string]$LiteralPath, [Parameter()][switch]$AccessOnly, [Parameter()][switch]$ModificationOnly ) if (-not (Test-Path $LiteralPath)) { $null = New-Item -ItemType File -Path $LiteralPath }; $item = Get-Item $LiteralPath; if (-not $ModificationOnly.IsPresent) { $item.LastAccessTime = [datetime]::now }; if (-not $AccessOnly.IsPresent) { $item.LastWriteTime = [datetime]::now }; Get-Item $LiteralPath }
+# function y { $tmp = [System.IO.Path]::GetTempFileName(); yazi $args --cwd-file="$tmp"; $cwd = Get-Content -Path $tmp -Encoding UTF8; if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) { Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd)) }; Remove-Item -Path $tmp }
+# function qq { if ($env:YAZI_LEVEL) { exit } else { Write-Error 'No Yazi instance detected' } }
+# function Get-PoshUpdates { Push-Location $profile/..; git pull; oh-my-posh upgrade; Pop-Location }
+function Watch-Command
+{
    [CmdletBinding()]
    param(
       [Parameter(Mandatory)]
@@ -46,11 +48,31 @@ function Watch-Command {
       $sum=$sum+2;
       $currVert = [System.Console]::CursorTop;
       $newPos = $currVert - $sum
-      if($newPos -ge 0){
+      if($newPos -ge 0)
+      {
          [System.Console]::SetCursorPosition(0,$currVert-$sum)
       }
       Start-Sleep -Seconds $period
    }
+}
+function Get-ShortcutTarget
+{
+   param(
+      [Parameter(Mandatory, ValueFromPipeline)]
+      [string]
+      $path
+   )
+   begin
+   {
+      $sh = New-Object -COM WScript.Shell
+   }
+   process
+   {
+      foreach($p in $path)
+      {
+         $sh.CreateShortcut($p).TargetPath
+      }
+   } 
 }
 New-Alias -Name watch -Value Watch-Command -ErrorAction SilentlyContinue
 #Invoke-Expression (& { (zoxide init powershell | Out-String) }) -EA SilentlyContinue
