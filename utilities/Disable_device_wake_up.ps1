60975a0497259bb7d6577c2b489a286060499a3e
Pedrokostam <Pedrokostam@outlook.com>
2026-02-22 00:11:09 +0100

    improvemnt to wake up

--- utilities/Disable_device_wake_up.ps1
+++ utilities/Disable_device_wake_up.ps1
@@ -2,15 +2,27 @@
 
 $devices = powercfg -devicequery wake_armed | ForEach-Object { $_.Trim()} | Where-Object Length
 
-if ($devices){
-   foreach ($device in $devices){
+if ($devices)
+{
+   $counter = 0;
+   foreach ($device in $devices)
+   {
+      if($device -eq 'NONE')
+      {
+         continue
+      }
       Write-host "Disabling ".. -NoNewline
       Write-host $device -NoNewline -ForegroundColor Cyan
       Write-host "... " -NoNewline
       powercfg -devicedisablewake $device
       Write-host "DONE" -ForegroundColor Green
+      $counter=$counter+1
    }
-}
-else{
+   if($counter -eq 0)
+   {
+      Write-host "No devices can wake-up the computer" -ForegroundColor Green
+   }
+} else
+{
    Write-host "No devices can wake-up the computer" -ForegroundColor Green
 }
