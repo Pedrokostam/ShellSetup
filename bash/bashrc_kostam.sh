7bc3c1b115917c853757920e0cfa5e82ce63b599
Maciej Krosta <pedrokostam@outlook.com>
2026-07-12 22:39:42 +0200

    fixed typo in eval :(

--- bash/bashrc_kostam.sh
+++ bash/bashrc_kostam.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CONFIG_FILE="$SCRIPT_DIR/../oh-my-posh/kostamfive.omp.json"
 eval "$(oh-my-posh init bash --config "$CONFIG_FILE")"
-eval "#(zoxide init bash)"
+eval "$(zoxide init bash)"
