0169839077fcf8067348b3218eada2dceba44564
Maciej Krosta <PedroKostam@outlook.com>
2026-08-03 19:37:53 +0200

    added color, actually

---
+++ python/color.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+class Color(Enum):
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    
+    BRIGHT_BLACK = "\033[90m"
+    BRIGHT_RED = "\033[91m"
+    BRIGHT_GREEN = "\033[92m"
+    BRIGHT_YELLOW = "\033[93m"
+    BRIGHT_BLUE = "\033[94m"
+    BRIGHT_MAGENTA = "\033[95m"
+    BRIGHT_CYAN = "\033[96m"
+    BRIGHT_WHITE = "\033[97m"
+
+    # Style Modifiers
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    
+    # Reset Sequence
+    RESET = "\033[0m"
+
+    def __str__(self):
+        """Allows direct usage in f-strings without typing '.value'."""
+        return self.value
+
+    def wrap(self,s:str)->str:
+        return color_wrap(s,self)
+
+    def print(self,s:str,*args,**kwargs):
+        color_print(s,self,*args,**kwargs)
+
+def wrap_color(s:str,color:Color)->str:
+    return f"{color}{s}{Color.RESET}"
+
+def color_print(s:str,color:Color,*args,**kwargs):
+    print(color_wrap(s,color),*args,**kwargs)
